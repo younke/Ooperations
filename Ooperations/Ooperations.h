209b151000c39657dd569d8ae9ed522e52c47145
younke <kewgram@gmail.com>
2020-02-18 22:31:10 +0900

    Initial Commit

---
+++ Ooperations/Ooperations.h
@@ -0,0 +1,19 @@
+//
+//  Ooperations.h
+//  Ooperations
+//
+//  Created by younke on 18.02.2020.
+//  Copyright © 2020 funjahmental. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Ooperations.
+FOUNDATION_EXPORT double OoperationsVersionNumber;
+
+//! Project version string for Ooperations.
+FOUNDATION_EXPORT const unsigned char OoperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ooperations/PublicHeader.h>
+
+
